ab8fcda15c354975ac56186094ef1e2614568686
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2024-08-15 12:22:05 +0530

    add : basic file structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Phishing-classifier
+This is a machine learning project which tells if a website is phishing or not
+
+
+''' pip install -e . '''
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vaishali Thakur",
+    author_email="vaishali3837@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
